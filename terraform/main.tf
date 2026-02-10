57f5b5fbeae57c71ba43ec08828e2df1bee1e152
Syed Ismail <syedismailpasha451@gmail.com>
2026-02-10 14:57:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Security Group
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_security_group" "allow_web" {
   name        = "allow-web-ssh"
   description = "Allow SSH, HTTP, Netdata"
@@ -42,13 +47,11 @@ resource "aws_security_group" "allow_web" {
   }
 }
 
-# Frontend EC2
 resource "aws_instance" "frontend" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
-  key_name      = "Ansible"
-
-  subnet_id              = "subnet-0b9fac4f91fd48a0c"
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2023
+  instance_type          = "t3.micro"
+  key_name               = "Ansible"
+  subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.allow_web.id]
 
   tags = {
@@ -56,13 +59,11 @@ resource "aws_instance" "frontend" {
   }
 }
 
-# Backend EC2
 resource "aws_instance" "backend" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t3.micro"
-  key_name      = "Ansible"
-
-  subnet_id              = "subnet-0b9fac4f91fd48a0c"
+  ami                    = "ami-053b0d53c279acc90" # Ubuntu 22
+  instance_type          = "t3.micro"
+  key_name               = "Ansible"
+  subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.allow_web.id]
 
   tags = {
